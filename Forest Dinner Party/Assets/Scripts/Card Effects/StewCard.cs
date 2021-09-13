031e37a460856dfb9328f8d10c95b498438a9e0d
JPoelk <71859906+JPoelk@users.noreply.github.com>
2021-09-13 21:54:16 +1200

    Stew
    Stew working now, takes objects as children

--- Forest Dinner Party/Assets/Scripts/Card Effects/StewCard.cs
+++ Forest Dinner Party/Assets/Scripts/Card Effects/StewCard.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class StewCard : MonoBehaviour
 {
     public DragDrop dragDrop; //The drag and drop script attached to this object
+    public GameObject stewTile;
 
     //Called every frame
     void Update()
     {
         if (dragDrop.isDropped) //When the object is dropped
         {
-            if (dragDrop.currentTarget.tag.Contains("Discard")) //When discarded
+            if (dragDrop.currentTarget.tag.Contains("|Discard|")) //When discarded
             {
                 dragDrop.currentTarget.GetComponent<MoveTile>().Activate(); //Use the discard effect
             }
@@ -29,18 +30,33 @@ public class StewCard : MonoBehaviour
         Transform parentSpace = dragDrop.currentTarget.transform.parent;
         GridManager gridManager = dragDrop.currentTarget.transform.parent.parent.GetComponent<GridManager>();
 
-        GameObject downLeft = gridManager.Down(gridManager.Left(parentSpace).transform.parent); //Find objects
-        GameObject upRight = gridManager.Up(gridManager.Right(parentSpace).transform.parent);
+        GameObject stew = Instantiate(stewTile, parentSpace);
+        Destroy(dragDrop.currentTarget);
 
-        if(downLeft.tag.Contains("|Food|"))
-        {
-            downLeft.transform.parent = dragDrop.currentTarget.transform;
-            downLeft.transform.
+        GameObject left = gridManager.Left(parentSpace); //Find objects
+        if (left){
+            GameObject downLeft = gridManager.Down(left.transform.parent);
+            if (downLeft)
+            {
+                if (downLeft.tag.Contains("|Food|"))
+                {
+                    downLeft.transform.SetParent(stew.transform);
+                    downLeft.transform.position = new Vector2(0, 0);
+                }
+            }
         }
-        if (upRight.tag.Contains("|Food|"))
-        {
 
-            upRight.transform.parent = dragDrop.currentTarget.transform;
+        GameObject right = gridManager.Right(parentSpace);
+        if (right) {
+            GameObject upRight = gridManager.Up(right.transform.parent);
+            if (upRight)
+            {
+                if (upRight.tag.Contains("|Food|"))
+                {
+                    upRight.transform.SetParent(stew.transform);
+                    upRight.transform.position = new Vector2(0, 0);
+                }
+            }
         }
     }
 }
